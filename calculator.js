8a1a09ff806749b821dd30ea7177d468f8b11123
AitorMP <aitor.martinez.pons@gmail.com>
2022-01-22 11:58:11 +0000

    done iterations 5, 6, 7

--- calculator.js
+++ calculator.js
@@ -1,14 +1,49 @@
 function sum(a, b) {
-  return;
+  if (a === undefined) {
+    a = 0;
+  }
+  if (b === undefined) {
+    b = 0;
+  }
+  if (a === undefined && b === undefined) {
+    a = 0;
+    b = 0;
+  }
+
+  return a + b;
 }
 
+
 function subtract(a, b) {
-  return;
+  if (a === undefined) {
+    a = 0;
+  }
+  if (b === undefined) {
+    b = 0;
+  }
+  if (a === undefined && b === undefined) {
+    a = 0;
+    b = 0;
+  }
+
+  return a - b;
 }
 
 function divide(a, b) {
-  return;
+  if (a === undefined) {
+    a = 0;
+  }
+  if (b === 0) {
+    throw new Error('An explanatory error message');
+  }
+  if (a === undefined && b === undefined) {
+    a = 0;
+    b = 0;
+  }
+
+  return a / b;
 }
+
 
 function multiply(a, b) {
   return a * b;
